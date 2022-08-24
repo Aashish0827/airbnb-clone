ad3624034e5597ad5f983d2cb7864067c2d8dd35
aashish patel <aashishpatel0827@gmail.com>
2022-08-24 13:27:14 +0530

    firt git temp

---
+++ precache-manifest.3f12be1bc91f394d76a6113b387de5a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "425fe85782ea565f9dca4b28587c0eaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "072783e9e40e77a991b3",
+    "url": "/static/css/2.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "38ae98ddcc55d7e11278",
+    "url": "/static/css/main.64d365e7.chunk.css"
+  },
+  {
+    "revision": "072783e9e40e77a991b3",
+    "url": "/static/js/2.e3110680.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.e3110680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38ae98ddcc55d7e11278",
+    "url": "/static/js/main.34265af5.chunk.js"
+  },
+  {
+    "revision": "9d9a4c99211d98368a0d",
+    "url": "/static/js/runtime-main.7b3982eb.js"
+  }
+]);
